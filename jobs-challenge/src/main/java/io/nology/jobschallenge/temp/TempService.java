178dc2aa4201664edfcb2ccb11570a88672f89b8
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2022-10-13 13:49:12 +1030

    removes circular references

--- jobs-challenge/src/main/java/io/nology/jobschallenge/temp/TempService.java
+++ jobs-challenge/src/main/java/io/nology/jobschallenge/temp/TempService.java
@@ -2,6 +2,7 @@ package io.nology.jobschallenge.temp;
 
 import io.nology.jobschallenge.exceptions.JobNotExistException;
 import io.nology.jobschallenge.job.Job;
+import io.nology.jobschallenge.job.JobRepository;
 import io.nology.jobschallenge.job.JobService;
 import java.time.LocalDate;
 import java.util.List;
@@ -18,7 +19,7 @@ public class TempService {
   private TempRepository tempRepository;
 
   @Autowired
-  private JobService jobService;
+  private JobRepository jobRepository;
 
   public Temp create(TempCreateDTO data) {
     Temp newTemp = new Temp(data.getFirstName(), data.getLastName(), null);
@@ -35,7 +36,7 @@ public class TempService {
       return this.tempRepository.findAll();
     }
 
-    Optional<Job> maybeJob = this.jobService.findById(jobId);
+    Optional<Job> maybeJob = this.jobRepository.findById(jobId);
 
     if (maybeJob.isEmpty()) {
       throw new JobNotExistException();
