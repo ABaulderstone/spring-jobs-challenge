9b8efa3c325ec5ca42c967c8027f8943d55e189a
Alex Baulderstone <46916203+ABaulderstone@users.noreply.github.com>
2024-08-23 14:31:49 +1000

    Update TempRepository.java

--- jobs-challenge/src/main/java/io/nology/jobschallenge/temp/TempRepository.java
+++ jobs-challenge/src/main/java/io/nology/jobschallenge/temp/TempRepository.java
@@ -6,11 +6,19 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface TempRepository extends JpaRepository<Temp, Long> {
-  @Query(
-    "SELECT t FROM Temp as t  LEFT JOIN t.jobs as j WHERE(j.endDate < :startDate or j is null)"
-  )
-  List<Temp> availableOn(@Param("startDate") LocalDate startDate);
-}
+@Query(
+    "SELECT DISTINCT t FROM Temp t " +
+    "LEFT JOIN t.jobs j " +
+    "WHERE t NOT IN (" +
+        "SELECT t2 FROM Temp t2 " +
+        "JOIN t2.jobs j2 " +
+        "WHERE " +
+        "(:jobStartDate < j2.endDate AND :jobEndDate > j2.startDate)" +
+    ")"
+)
+List<Temp> availableForJob(
+    @Param("jobStartDate") LocalDate jobStartDate, 
+    @Param("jobEndDate") LocalDate jobEndDate
+);
 // @Query("SELECT j FROM Job j WHERE j.temp <> null")
 // List<Job> findAllAssigned();
